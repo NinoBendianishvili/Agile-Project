58f56de5841eb646bc6a0b92089295afb124a1c1
beeabo <102155590+NinoBendianishvili@users.noreply.github.com>
2024-06-17 12:34:41 +0400

    Update image_loader.py

--- MSI1/Loaders/image_loader.py
+++ MSI1/Loaders/image_loader.py
@@ -4,7 +4,6 @@ def fetch_image_from_url(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            # Check content type if needed: response.headers['Content-Type']
             return response.content
         else:
             print(f"Failed to fetch image: {response.status_code}")
